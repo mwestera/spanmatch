39d63f3a154381b56e5d59b5fe5b5044c7a71091
mwestera <m.westera@hum.leidenuniv.nl>
2024-09-27 19:32:34 +0200

    add tokenwise tests

--- spanmatch/test/test_main.py
+++ spanmatch/test/test_main.py
@@ -13,23 +13,23 @@ CDE = (2, 5)
 IJ = (8, 10)
 
 cases_multi = [
-    ([ABC, FG], [[ABC, FG], [KLM]], 0, (EXACT, 0)),
-    ([FG, ABC], [[ABC, FG], [KLM]], 0, (EXACT, 0)),
-    ([FG, ABC], [[KLM], [ABC, FG]], 0, (EXACT, 1)),
-    ([ABC, FG], [[KLM], [BC, FG]], 0, (SUPERSPAN, None)),
-    ([ABC, FG], [[KLM], [CDE, FG]], 0, (OVERLAP, None)),
-    ([ABC], [[KLM], [BC]], 0, (SUPERSPAN, None)),
-    ([BC], [[KLM], [ABC]], 0, (SUBSPAN, None)),
-    ([ABC], [[KLM], [BC]], 2, (EXACT, 1)),
-    ([BC], [[KLM], [ABC]], 1, (EXACT, 1)),
-    ([ABC, FG], [[KLM], [BC, IJ]], 0, (OVERLAP, None)),
-    ([BC, FG], [[KLM], [ABC, IJ]], 0, (OVERLAP, None)),
-    ([ABC, FG], [[KLM], [CDE, IJ]], 0, (OVERLAP, None)),
-    ([], [[KLM], [ABC, FG]], 0, (SUBSPAN, None)),
-    ([], [[KLM], []], 1, (EXACT, 1)),
-    ([ABC, FG], [[BC, FG], [KLM]], 1, (EXACT, 0)),
-    ([ABCDE], [[KLM], [BC, IJ]], 2, (SUPERSPAN, None)),
-    ([BC], [[KLM], [ABCDE]], 1, (SUBSPAN, None)),
+    ([ABC, FG], [[ABC, FG], [KLM]], 0, (EXACT, 0, None)),
+    ([FG, ABC], [[ABC, FG], [KLM]], 0, (EXACT, 0, None)),
+    ([FG, ABC], [[KLM], [ABC, FG]], 0, (EXACT, 1, None)),
+    ([ABC, FG], [[KLM], [BC, FG]], 0, (SUPERSPAN, None, None)),
+    ([ABC, FG], [[KLM], [CDE, FG]], 0, (OVERLAP, None, None)),
+    ([ABC], [[KLM], [BC]], 0, (SUPERSPAN, None, None)),
+    ([BC], [[KLM], [ABC]], 0, (SUBSPAN, None, None)),
+    ([ABC], [[KLM], [BC]], 2, (EXACT, 1, None)),
+    ([BC], [[KLM], [ABC]], 1, (EXACT, 1, None)),
+    ([ABC, FG], [[KLM], [BC, IJ]], 0, (OVERLAP, None, None)),
+    ([BC, FG], [[KLM], [ABC, IJ]], 0, (OVERLAP, None, None)),
+    ([ABC, FG], [[KLM], [CDE, IJ]], 0, (OVERLAP, None, None)),
+    ([], [[KLM], [ABC, FG]], 0, (SUBSPAN, None, None)),
+    ([], [[KLM], []], 1, (EXACT, 1, None)),
+    ([ABC, FG], [[BC, FG], [KLM]], 1, (EXACT, 0, None)),
+    ([ABCDE], [[KLM], [BC, IJ]], 2, (SUPERSPAN, None, None)),
+    ([BC], [[KLM], [ABCDE]], 1, (SUBSPAN, None, None)),
 ]
 
 
@@ -69,4 +69,21 @@ def test_match_spans(case):
 
     span1, span2, margin, target = case
     result = match_spans_categorical(span1, span2, margin=margin)
+    assert result == target
+
+
+
+span_BC = [(2, 6)]
+span_CDE = [(4, 10)]
+span_C = [(4, 6)]
+cases_tokenwise = [
+    (span_BC, span_CDE, 'a b c d e f g h i j', {'precision': 1/2, 'recall': 1/3, 'f1': 2 / 5, 'count': 10}),
+    (span_BC, span_C, 'a b c d e f g h i j', {'precision': 1/2, 'recall': 1.0, 'f1': 2 / 3, 'count': 10})
+]
+
+
+@pytest.mark.parametrize("case", cases_tokenwise)
+def test_match_spans_tokenwise(case):
+    span1, span2, text, target = case
+    result = match_spans_tokenwise(span1, span2, text)
     assert result == target
